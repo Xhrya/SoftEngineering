ee7a8f4b2bd6b08f63894bfcf277606a24b229e4
root <root@ip-172-31-32-11.ec2.internal>
2024-03-20 20:36:18 +0000

    complete account apis

--- src/routes/account.js
+++ src/routes/account.js
@@ -1,8 +1,10 @@
+const url_lib = require('url');
 const path = require('path');
 const jwt = require('jsonwebtoken');
-require('dotenv').config({ path: path.join(__dirname, "../cred.env") });
+require('dotenv').config({ path: path.join(__dirname, "../../cred.env") });
 
 const db = require(path.join(__dirname, "../tools/db.js"));
+const account_tools = require(path.join(__dirname, "../tools/account_tools.js"));
 
 const router = (req, res) => {
 	let { url } = req;
@@ -60,11 +62,13 @@ const router = (req, res) => {
 				try {
 					let body = JSON.parse(data);
 					//verify token and get user data
-					jwt.verify(body.token, process.env.JWT_SECRET, (err, decoded) => {
-						//handle error (wrong secret, improper format)
-						if (err) throw err;
+					account_tools.verify(body.token)
+					.then((data) => {
 						res.writeHead(200, { 'Content-Type': 'application/json' });
 						res.end(JSON.stringify(decoded));
+					})
+					.catch((error) => {
+						//handle error
 					})
 				} catch(e) {
 					//error with JSON
@@ -72,6 +76,81 @@ const router = (req, res) => {
 				}
 			})
 		}
+	} else if (url == '/account/create') {
+		//check if user is signed in first and verify admin permission
+		let auth_token = req.headers['authorization'];
+		if (auth_token) {
+			account_tools.verify(auth_token)
+			.then((data) => {
+				if (data.role == 0) {
+					if (req.method === 'POST') {
+						let req_body = '';
+						req.on('data', (chunk) => {
+							req_body += chunk;
+						})
+						req.on('end', () => {
+							try {
+								let body = JSON.parse(req_body);
+								let {
+									username, 
+									password,
+									email,
+									role
+								} = body;
+								//validate each entry and hash password
+								let q = `INSERT INTO User VALUES (default, '${username}', '${email}', '${password}', 'active', ${role})`; 
+								db.query(q, (err, results) => {
+									//handle error
+									if (err) throw err;
+									let obj = { success: true };
+									res.end(JSON.stringify(obj));
+								})
+							} catch(e) {
+								//error JSON
+							}
+						})
+					} else {
+						//wrong method
+					}
+				} else {
+					//unauthorized
+				}
+			})
+		}
+	} else if (url = '/account/view') {
+		let auth_token = req.headers['authorization'];
+		if (auth_token) {
+			account_tools.verify(auth_token)
+			.then((data) => {
+				if (data.role == 0) {
+					if (req.method === 'GET') {
+						let parsed_url = url_lib.parse(req.url, true);
+						let query_params = parsed_url.query;
+						if (query_params.user_id) {
+							let q = `SELECT * FROM User WHERE user_id = '${query_params.user_id}'`;
+							db.query(q, (err, results) => {
+								//handle err
+								if (err) throw err;
+								if (results.length > 0) {
+									let obj = results[0];
+									res.end(JSON.stringify(obj));
+								} else {
+									//bad user id
+								}		
+							})
+						} else {
+							//user_id not specified
+						}	
+					} else {
+						//wrong method
+					}
+				} else {
+					//unauthorized
+				}
+			})
+		} else {
+			//unauthorized
+		}			
 	}
 }
 
