85559e268076415b3ad34f6fc0b596b54ec17f8d
Ayaan Qayyum <96453808+qayyumayaan@users.noreply.github.com>
2024-04-27 15:28:17 -0400

    Delete useless files

--- src/routes/admin.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-const db = require(path.join(__dirname, "../tools/db.js"));
-
-const router = (req, res) => {
-	res.end('Hello admin!');
-}
-
-module.exports = router;
