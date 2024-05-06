911ebd6d4aa73cc643b3467b18f54b16b9eaf3e9
anmpat <anuvpatel15@gmail.com>
2024-05-05 20:15:15 -0400

    newstuff

--- HelpdeskApi/src/app.js
+++ HelpdeskApi/src/app.js
@@ -1,15 +1,23 @@
 const path = require('path');
 const http = require('http');
-require('dotenv').config({ path: path.join(__dirname, "../cred.env") });
 const fs = require('fs');
+require('dotenv').config({ path: path.join(__dirname, "../../env/cred.env") });
 
-const port = process.env.PORT;
+const port = 3000;
 
-const questions_routes = require(path.join(__dirname, "./routes/questions.js"));
+const admin_routes = require(path.join(__dirname, "./routes/admin.js"));
 const account_routes = require(path.join(__dirname, "./routes/account.js"));
+const flag_routes = require(path.join(__dirname, "./routes/flag.js"));
+const ban_routes = require(path.join(__dirname, "./routes/ban.js"));
+const gpt_routes = require(path.join(__dirname, "./routes/gpt.js"));
+const payment_routes = require(path.join(__dirname, "./routes/payments.js"));
+const search_routes = require(path.join(__dirname, "./routes/search.js"));
+const seller_routes = require(path.join(__dirname, "./routes/seller.js"));
 const ticket_routes = require(path.join(__dirname, "./routes/ticket.js"));
 const helpdesk_routes = require(path.join(__dirname, "./routes/helpdesk.js"));
-
+const questions_routes = require(path.join(__dirname, "./routes/questions.js"));
+const order_routes = require(path.join(__dirname, "./routes/orders.js"));
+const cart_routes = require(path.join(__dirname, "./routes/cart.js"));
 
 const server = http.createServer((req, res) => {
 	//retrieve 'url' hitting the server
@@ -19,14 +27,32 @@ const server = http.createServer((req, res) => {
 
 	if (route == 'account') {
 		account_routes(req, res);
+	} else if (route == 'admin') {
+		admin_routes(req, res);
+	} else if (route == 'flags') {
+		flag_routes(req, res);
+	} else if (route == 'ban') {
+		ban_routes(req, res);
+	} else if (route == 'gpt') {
+		gpt_routes(req, res);
+	} else if (route == 'payments') {
+		payment_routes(req, res);
+	} else if (route == 'search') {
+		search_routes(req, res);
+	} else if (route == 'orders') {
+		order_routes(req, res);
+	} else if (route == 'seller') {
+		seller_routes(req, res);
 	} else if (route == 'questions') {
 		questions_routes(req, res);
 	} else if (route == 'ticket') {
 		ticket_routes(req, res);
 	} else if (route == 'helpdesk') {
 		helpdesk_routes(req, res);
-	} else if (route == 'google') {
-		fs.readFile(path.join(__dirname, 'google_login.html'), (err, data) => {
+	} else if (route == 'cart') {
+		cart_routes(req, res);
+	} else if (route == '') {
+		fs.readFile(path.join(__dirname, '../templates/views/google_login.html'), (err, data) => {
             if (err) {
                 res.writeHead(500);
                 res.end('Error reading googlelogin.html file');
@@ -36,7 +62,8 @@ const server = http.createServer((req, res) => {
             res.end(data);
         });
 	} else {
-		res.end('Welcome to MunchMate!');
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('This URL is not found.');		
 	}
 })
 
